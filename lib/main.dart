107fbb99a4ed242ec2c8cc42a4beda712373ea99
mrfrolk <ekaterita20@gmail.com>
2025-03-06 08:58:38 +0500

    1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const TextSaverPage());
+    return const MaterialApp(home: TextSaverPage());
   }
 }
 
