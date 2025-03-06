be877bbc13373333a6da6169370cf18c8f1208c2
mrfrolk <ekaterita20@gmail.com>
2025-03-06 06:40:54 +0500

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,13 +13,51 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("My App")));
-  }
+  State<Home> createState() => _HomeState();
 }
 
+class _HomeState extends State<Home> {
+  final _controller = TextEditingController();
+  String textValue = "";
 
+  @override
+  void initState() {
+    _controller.addListener(textHandle);
+    super.initState();
+  }
+
+  void textHandle() {
+    setState(() {
+      textValue = _controller.text.trim();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  labelText: "Имя",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Text("$textValue", style: TextStyle(fontSize: 30)),
+          ],
+        ),
+      ),
+    );
+  }
+}
